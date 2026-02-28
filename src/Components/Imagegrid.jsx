1a02cb2903b4f3be0363248df5d37d4a9f5e2255
surbhi <surbhipansuriya633@gmail.com>
2026-02-28 09:59:49 +0530

    remove one image from imagegrid

--- src/Components/Imagegrid.jsx
+++ src/Components/Imagegrid.jsx
@@ -11,14 +11,14 @@ const images = [
     // { src: require("../Images/last2nd_image.jpg"), alt: "Image 8", className: "smallimage" },
     // { src: require("../Images/untitled-1-17.jpg"), alt: "Image 9", className: "smallimage" },
 
-    { src: require("../Images/2026 photos/web 3.jpg.jpeg"), alt: "Image 1", className: "col-span-1 row-span-2 imgheightclass " },
+    // { src: require("../Images/2026 photos/web 3.jpg.jpeg"), alt: "Image 1", className: "col-span-1 row-span-2 imgheightclass " },
+    { src: require("../Images/2026 photos/web 12.jpg.jpeg"), alt: "Image 6", className: "row-span-2  col-span-2 imgheightclass" },
     { src: require("../Images/2026 photos/web 6 (1).jpg.jpeg"), alt: "Image 2", className: "smallimage" },
     { src: require("../Images/2026 photos/web 9.JPG.jpeg"), alt: "Image 3", className: "smallimage" },
-    { src: require("../Images/2026 photos/web 12.jpg.jpeg"), alt: "Image 6", className: "row-span-1  col-span-2 imgheightclass" },
     { src: require("../Images/2026 photos/web (1).jpg.jpeg"), alt: "Image 4", className: "smallimage" },
+    { src: require("../Images/2026 photos/web 5.jpg.jpeg"), alt: "Image 7", className: "col-span-2 row-span-2 imgheightclass" },
     { src: require("../Images/2026 photos/web (3).jpg.jpeg"), alt: "Image 5", className: "smallimage" },
     { src: require("../Images/2026 photos/web 10.jpg.jpeg"), alt: "Image 5", className: "smallimage" },
-    { src: require("../Images/2026 photos/web 5.jpg.jpeg"), alt: "Image 7", className: "col-span-2 row-span-2 imgheightclass" },
     { src: require("../Images/2026 photos/web (1).jpg.jpeg"), alt: "Image 8", className: "smallimage" },
     { src: require("../Images/2026 photos/web (4).jpg.jpeg"), alt: "Image 9", className: "smallimage" },
 ];
