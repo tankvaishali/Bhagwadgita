7cc1ab33c72f55d4046daea0b1ed32859d959b32
surbhi <surbhipansuriya633@gmail.com>
2026-04-08 18:31:30 +0530

    solve warning

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home';
-import Buynow from './Components/Buynow';
+// import Buynow from './Components/Buynow';
 import Datalist from './Components/Datalist';
 
 function App() {
